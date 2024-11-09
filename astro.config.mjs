afed3ed8b0bf78c698efe3024d759767c4be5c1a
Juan Pablo Aguilar <juan.aguilarl@unmsm.edu.pe>
2024-11-09 08:01:02 -0500

    aaaaaa

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://thesunfall.github.io/',
-    base: 'blog'
+    base: '/blog'
 });
